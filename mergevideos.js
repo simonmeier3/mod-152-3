4545038f11167a7ce69de25c83fa9b6160811310
Simon Meier <72375787+simonmeier3@users.noreply.github.com>
2021-03-29 17:55:10 +0200

    Delete mergevideos.js

--- mergevideos.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ffmpeg = require("fluent-ffmpeg");
-ffmpeg()
-    .mergeAdd(process.cwd() + '/Big_Buck_Bunny_1.mp4')
-    .mergeAdd(process.cwd() + '/Big_Buck_Bunny_2.mp4')
-    .mergeToFile(process.cwd() + '/merged.mp4');
-//# sourceMappingURL=mergevideos.js.map
