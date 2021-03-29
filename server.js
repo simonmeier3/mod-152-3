182de32b45db13b90fc491a9094b3f81d189abb0
Simon Meier <simon.meier@basenet.ch>
2021-03-29 17:52:16 +0200

    implemented config params

--- server.js
+++ server.js
@@ -1,46 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var multer = require("multer");
 var ffmpeg = require("fluent-ffmpeg");
 var fs = require('fs');
 var uploadFolder = './uploads';
+var path = require('path');
+var ext;
 var app = express();
 var store = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -51,9 +17,14 @@ var store = multer.diskStorage({
     }
 });
 var upload = multer({ storage: store });
+var fileName = '';
 app.post('/api/videos', upload.array('files'), function (req, res, next) {
-    res.json("Done");
     var videos = fs.readdirSync(uploadFolder);
+    var fileNameParam = req.query.fileName;
+    var turnParam = req.query.turn;
+    var widthParam = req.query.width;
+    var heightParam = req.query.height;
+    var bitrateParam = req.query.videoBitrate;
     var ffmpegvideos = ffmpeg();
     videos.forEach(function (file) {
         if (file === '.gitkeep') {
@@ -62,21 +33,72 @@ app.post('/api/videos', upload.array('files'), function (req, res, next) {
         else {
             ffmpegvideos = ffmpegvideos.mergeAdd(process.cwd() + '/uploads/' + file);
             console.log(file);
+            ext = path.extname(__dirname + 'mergedVideo' + file);
+            console.log(ext);
+            fileName = "Uploaded_Video" + ext;
         }
     });
-    mergeVideos().then(function (r) {
-        console.log("finished merging Videos");
-    });
-    function mergeVideos() {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                ffmpegvideos
-                    .mergeToFile(process.cwd() + '/merged.mp4');
-                console.log(process.cwd() + '/merged.mp4');
-                return [2 /*return*/];
-            });
+    if (fileNameParam) {
+        fileName = fileNameParam + ext;
+        console.log("Changed Filename to " + fileName);
+    }
+    ffmpegvideos.on('error', function (err) {
+        console.log(err);
+    })
+        .on('end', function () {
+        console.log('finished with merge file' + fileName);
+        editVideo();
+    })
+        .mergeToFile(process.cwd() + '/mergedVideo/' + fileName);
+    console.log("Waiting for Video to finish merging");
+    function editVideo() {
+        ffmpegvideos = ffmpeg();
+        ffmpegvideos.input(process.cwd() + '/mergedVideo/' + fileName);
+        if (turnParam === 'yes') {
+            ffmpegvideos
+                .withVideoFilter('transpose=1, transpose=1');
+            console.log("Added turn Param");
+        }
+        if (bitrateParam) {
+            ffmpegvideos
+                .videoBitrate(bitrateParam);
+            console.log("Added Bitrate Param");
+        }
+        if (heightParam && !widthParam) {
+            ffmpegvideos
+                .size('?x' + heightParam);
+            console.log("Added height Param");
+        }
+        if (widthParam && !heightParam) {
+            ffmpegvideos
+                .size(widthParam + 'x?');
+            console.log("Added width Param");
+        }
+        if (widthParam && heightParam) {
+            ffmpegvideos
+                .size(widthParam + 'x' + heightParam);
+            console.log("Added width and height Param");
+        }
+        ffmpegvideos.on('error', function (err) {
+            console.log(err);
+        })
+            .on('end', function () {
+            console.log('finished editing Video');
+            response();
+        });
+        ffmpegvideos.saveToFile(process.cwd() + '/editedVideo/' + fileName);
+        console.log("Waiting for Video to finish editing");
+    }
+    function response() {
+        res.json({
+            data: {
+                video: {
+                    location: "http://localhost:3000/mod-152-3/editedVideo/" + fileName
+                }
+            }
         });
     }
 });
+app.use(express.static('editedVideo'));
 app.listen(process.env.PORT || 3000);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ import * as ffmpeg from "fluent-ffmpeg";
 
 const fs = require('fs');
 const uploadFolder = './uploads';
+const path = require('path');
+let ext;
 let app = express();
 
 let store = multer.diskStorage({
@@ -16,15 +18,19 @@ let store = multer.diskStorage({
 });
 
 let upload = multer({storage: store});
+let fileName = '';
 
 
 app.post('/api/videos', upload.array('files'), (req,res,next ) => {
-    res.json("Done")
 
     const videos = fs.readdirSync(uploadFolder);
+    let fileNameParam: any = req.query.fileName;
+    let turnParam: any = req.query.turn;
+    let widthParam: any = req.query.width;
+    let heightParam: any = req.query.height;
+    let bitrateParam: any = req.query.videoBitrate;
 
     let ffmpegvideos = ffmpeg();
-
     videos.forEach(file => {
         if(file === '.gitkeep'){
             return
@@ -32,31 +38,79 @@ app.post('/api/videos', upload.array('files'), (req,res,next ) => {
         else {
             ffmpegvideos = ffmpegvideos.mergeAdd(process.cwd() + '/uploads/' + file);
             console.log(file);
+            ext = path.extname(__dirname + 'mergedVideo' + file);
+            console.log(ext);
+            fileName = "Uploaded_Video" + ext;
+
         }
     });
 
-    mergeVideos().then(r => {
-        console.log("finished merging Videos")
-    });
+    if(fileNameParam){
+        fileName = fileNameParam + ext;
+        console.log("Changed Filename to " + fileName);
+    }
 
-    async function mergeVideos() {
-        ffmpegvideos
-            .mergeToFile(process.cwd() + '/merged.mp4');
-            console.log(process.cwd() + '/merged.mp4');
+    ffmpegvideos.on('error', (err) => {
+            console.log(err);
+        })
+        .on('end', () => {
+            console.log('finished with merge file' + fileName);
+            editVideo();
+        })
+        .mergeToFile(process.cwd() + '/mergedVideo/' + fileName);
+        console.log("Waiting for Video to finish merging")
+
+    function editVideo(){
+        ffmpegvideos = ffmpeg();
+        ffmpegvideos.input(process.cwd() + '/mergedVideo/' + fileName)
+        if(turnParam === 'yes'){
+            ffmpegvideos
+                .withVideoFilter('transpose=1, transpose=1')
+            console.log("Added turn Param");
+        }
+        if(bitrateParam){
+            ffmpegvideos
+                .videoBitrate(bitrateParam);
+            console.log("Added Bitrate Param");
+        }
+        if(heightParam && !widthParam){
+            ffmpegvideos
+                .size('?x' + heightParam)
+            console.log("Added height Param");
+        }
+        if(widthParam && !heightParam){
+            ffmpegvideos
+                .size(widthParam + 'x?')
+            console.log("Added width Param");
+        }
+        if(widthParam && heightParam){
+            ffmpegvideos
+                .size(widthParam + 'x' + heightParam)
+            console.log("Added width and height Param");
+        }
+        ffmpegvideos.on('error', (err) => {
+            console.log(err);
+        })
+            .on('end', () => {
+                console.log('finished editing Video');
+                response();
+            })
+        ffmpegvideos.saveToFile(process.cwd() + '/editedVideo/' + fileName);
+        console.log("Waiting for Video to finish editing");
     }
 
     function response() {
         res.json({
                 data: {
                     video: {
-                        location: "http://localhost/api/video/merged.mp4"
+                        location: "http://localhost:3000/mod-152-3/editedVideo/" + fileName
                     }
                 }
             }
         );
     }
-
-
 });
+
+app.use(express.static('editedVideo'));
 
 app.listen(process.env.PORT || 3000);
